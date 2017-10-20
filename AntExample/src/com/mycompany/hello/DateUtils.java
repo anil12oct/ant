3f624cf6966e37984eb72f6d429d4598cb90b46d
anil9392 <anilkumaranu.gorantla@gmail.com>
2017-10-20 17:22:16 +0530

    added

---
+++ AntExample/src/com/mycompany/hello/DateUtils.java
@@ -0,0 +1,15 @@
+package com.mycompany.hello;
+
+import java.util.Date;
+
+public class DateUtils {
+
+	public static void main(String[] args) {
+
+		System.out.println(new Date());
+		System.out.println("Hello Student!! Welcome to build tools");
+		
+	}
+
+
+}
